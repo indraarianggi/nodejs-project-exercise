29e17197ca5e3ca1fbe5cb66c66c6c36fe0e4c56
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2018-03-12 07:23:11 +0700

    reduce & remove cart

--- shopping-cart/models/cart.js
+++ shopping-cart/models/cart.js
@@ -14,6 +14,23 @@ module.exports = function Cart(oldCart) {
         storedItem.price = storedItem.item.price * storedItem.qty;
         this.totalQty++;
         this.totalPrice += storedItem.item.price;
+    };
+
+    this.reduceOne = function(id) {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price;
+
+        if (this.items[id].qty <= 0) {
+            delete this.items[id];
+        }
+    };
+
+    this.removeItem = function(id) {
+        this.totalQty -= this.items[id].qty;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
     }
 
     this.generateArray = function() {
@@ -23,5 +40,5 @@ module.exports = function Cart(oldCart) {
         }
 
         return arr;
-    }
+    };
 };
